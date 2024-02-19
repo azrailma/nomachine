827069c85b06580fb6d05739b70b4c573f1d2eae
Azra Nurul Ilma <136613890+azrailma@users.noreply.github.com>
2024-02-19 15:49:55 +0700

    Create ngrok.sh

---
+++ ngrok.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+stty intr ""
+stty quit ""
+stty susp undef
+
+clear
+rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
+echo "======================="
+echo "Downloading ngrok..."
+echo "======================="
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
+read -p "Paste Ngrok Authtoken: " CRP
+ngrok config add-authtoken $CRP
